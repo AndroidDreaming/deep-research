201e54113f5759b3d94f1a0a2dccaa9c67c06280
AndroidDreaming <339291012@qq.com>
2025-05-22 14:28:33 +0800

    升级

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ function Home() {
       <footer className="my-4 text-center text-sm text-gray-600 print:hidden">
         <a href="https://github.com/AndroidDreaming/" target="_blank">
           {t("copyright", {
-            name: "Dreamy",
+            name: "Dreamy萧",
           })}
         </a>
       </footer>
